a373465b74441593f301d97609d8032165b1ad69
viniiirj <vsr.rio@gmail.com>
2023-10-05 21:49:36 -0300

    testando metodo post

--- src/server.js
+++ src/server.js
@@ -1,20 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id , user } = request.params
+app.post('/users', (request, response) => {
 
-    response.send(`
-        Id da mensagem: ${id}.
-        Para o usuário: ${user}.
-    `)
-});
-
-app.get('/users', (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send( `Página: ${page}. Mostrar: ${limit}`)
+    const { name, email, senha } = request.body
+    response.json({ name, email, senha })
 })
 
 const PORT = 3333;
