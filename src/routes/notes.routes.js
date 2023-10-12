c5dc67a5085438b8ad6a1549b4daf737caa20079
viniiirj <vsr.rio@gmail.com>
2023-10-11 21:46:06 -0300

    Update notes.routes.js

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,9 +7,9 @@ const notesRoutes = Router()
 
 const notesController = new NotesController();
 
-notesRoutes.post('/:user_id', notesController.create)
-notesRoutes.get('/:id', notesController.show)
-notesRoutes.delete('/:id', notesController.delete)
+notesRoutes.post('/:user_id', notesController.create);
+notesRoutes.get('/:id', notesController.show);
+notesRoutes.delete('/:id', notesController.delete);
 
 
 module.exports = notesRoutes;
