dede0863e8ce157a68d0d20009f262d5f8f30867
viniiirj <vsr.rio@gmail.com>
2023-10-30 20:38:39 -0300

    Avatar no bd

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express")
 
 const UsersController = require('../controllers/UsersController');
+const UserAvatarController = require("../controllers/UserAvatarController");
+
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 const multer = require('multer')
 const uploadConfig = require('../configs/upload')
@@ -8,13 +10,11 @@ const uploadConfig = require('../configs/upload')
 const usersRoutes = Router()
 const upload = multer(uploadConfig.MULTER)
 
+const userAvatarController = new UserAvatarController();
 const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), (request, response) => {
-    console.log(request.file.filename);
-    response.json()
-})
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 module.exports = usersRoutes;
